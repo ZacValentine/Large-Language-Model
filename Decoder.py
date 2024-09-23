ead8cd9bc6ba171d90b0ed276ec0c6082a2e7e39
Zac Valentine <112518832+ZacValentine@users.noreply.github.com>
2024-09-23 19:38:18 -0400

    Add files via upload

---
+++ Decoder.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+from DecoderBlock import DecoderBlock
+
+class Decoder(nn.Module):
+    def __init__(self, embed_dim, num_blocks, num_heads, ff_dim, batch_size):
+        super(Decoder, self).__init__()
+        self.blocks = nn.ModuleList([DecoderBlock(embed_dim, num_heads, ff_dim, batch_size) for _ in range(num_blocks)])
+        
+    def forward(self, sequence, key_padding_mask, causal_mask):
+        for block in self.blocks:
+            sequence = block(sequence, key_padding_mask, causal_mask)
+        return sequence
+
---
+++ PositionalEncoding.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import math
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, embed_dim)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, embed_dim, 2) * -(math.log(10000.0) / embed_dim))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + Variable(self.pe[:x.shape[1]], requires_grad=False)
+        return x
