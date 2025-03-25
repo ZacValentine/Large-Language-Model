e0f0e21964df70e778f8c2a62bd7ba76dbeb9521
Zac Valentine <112518832+ZacValentine@users.noreply.github.com>
2025-03-25 17:09:28 -0400

    Update train.py

--- train.py
+++ train.py
@@ -23,9 +23,7 @@ model, start_step, losses = load_model_from_checkpoint(config["model_folder"] + 
 train_df = pd.read_csv(config["dataset_path"],lineterminator='\n')
 num_steps = train_df.shape[0]
 criterion = nn.CrossEntropyLoss()
-# criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-optimizer = optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"]) # weight decay is L2 reg
-# best_loss_interval = 10 //<- how many regular intervals between this interval
+optimizer = optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"]) 
 model.train()
 
 for epoch in range(config["num_epochs"]):
@@ -37,11 +35,6 @@ for epoch in range(config["num_epochs"]):
         logits, probs = model(sequence[:, :-1], key_padding_mask = None, causal_mask = causal_mask)
 
         loss = criterion(logits.view(-1, vocab_size), sequence[:, 1:].view(-1))
-
-        # attention_matrix = model.decoder.blocks[0].attention.last_attention
-        # for i in range(1, 6):
-        #     attention_matrix = torch.cat((attention_matrix, model.decoder.blocks[i].attention.last_attention))
-        # loss += entropy_loss(attention_matrix, config["entropy_weight"])
 
         loss.backward()
         optimizer.step()
